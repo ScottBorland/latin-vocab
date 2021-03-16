4392e8524b46237f97a4315a2304dfe7eeed2291
johndoe2196 <johndoe219656@gmail.com>
2021-03-16 19:59:49 +0000

    Updates

---
+++ precache-manifest.42437d36eec5fd573972e1ac46cf1617.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5468e14f262e7675aedd6970414c7577",
+    "url": "/latin-vocab/index.html"
+  },
+  {
+    "revision": "10489aadbfd607187e07",
+    "url": "/latin-vocab/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "c578dac1df2f8e3ba8b6",
+    "url": "/latin-vocab/static/css/main.969982cf.chunk.css"
+  },
+  {
+    "revision": "10489aadbfd607187e07",
+    "url": "/latin-vocab/static/js/2.d205f3b2.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/latin-vocab/static/js/2.d205f3b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c578dac1df2f8e3ba8b6",
+    "url": "/latin-vocab/static/js/main.e128d47e.chunk.js"
+  },
+  {
+    "revision": "792a4c047b80643809db",
+    "url": "/latin-vocab/static/js/runtime-main.c24893f3.js"
+  }
+]);
